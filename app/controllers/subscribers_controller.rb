29970050b0668338754da5cdf5b9f7f37b168268
chikiriau-Olesya <faceremirabilia@gmail.com>
2020-12-14 20:20:01 +0300

    suraface first commit

---
+++ app/controllers/subscribers_controller.rb
@@ -0,0 +1,21 @@
+class SubscribersController < ApplicationController
+  def index
+    @subscribers = Subscriber.all
+  end
+
+#new subscriber
+  def create
+    @subscriber = Subscriber.new(subscriber_params)
+    if @subscriber.save
+      redirect_to about_path, notice: "Ура!"
+    else
+      redirect_to about_path, notice: "Не ура("
+    end
+  end
+
+  private
+
+    def subscriber_params
+      params.require(:subscriber).permit(:email)
+    end
+end
