b6aaebc2db6827cf5152f560a2e06cf43498ad08
chikiriau-Olesya <faceremirabilia@gmail.com>
2020-12-19 21:56:51 +0300

    add landing page WIP adaprive

---
+++ app/javascript/packs/landing.js
@@ -0,0 +1,16 @@
+console.log(1)
+// 
+// let btnOpen = document.querySelector('.landing-header__rect')
+// let menu = document.querySelector('.landing-header__menu')
+// let btnClose = document.querySelector('.landing-header__rect-close')
+//
+// btnOpen.onclick = function() {
+//     menu.style.display = 'block'
+//     btnOpen.classList.add('landing-header__rect-close')
+//     console.log(2)
+// }
+
+// btnClose.onclick = function() {
+//     menu.style.display = 'none'
+//     btnOpen.classList.remove('landing-header__rect-close')
+// }
