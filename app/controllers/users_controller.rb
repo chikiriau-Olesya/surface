4d8765c592df7ddf42b6d193d1a8321cdad2fe65
chikiriau-Olesya <faceremirabilia@gmail.com>
2021-03-13 16:58:55 +0300

    ops, edit show of users post in profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = current_user.posts.order(created_at: :desc)
+    @posts = @user.posts.order(created_at: :desc)
     if @user
       @posts = @user.favorited_posts
       render actions: :show
