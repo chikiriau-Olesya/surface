6bc2c002f51476b1ef245134c1f564136d99d4e6
KathernZ <zyrianovakate10@gmail.com>
2021-03-25 11:59:35 +0300

    added btn

---
+++ app/javascript/components/A_Btn.js
@@ -0,0 +1,41 @@
+import React from "react"
+import PropTypes from "prop-types"
+import 'stylesheets/button.scss'
+// import Arrow from 'images/arrow-btn.svg'
+
+const STYLES = [
+  'btn--main',
+  'btn--secondary',
+  'btn--icons'
+]
+
+const STATES = [
+  'btn--primary',
+  'btn--hover',
+  'btn--pressed',
+  'btn--disabled'
+]
+
+const A_Btn = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonState
+}) => {
+
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+  ? buttonStyle : STYLES[0]
+
+  const checkButtonState = STATES.includes(buttonState)
+  ? buttonState : STATES[0]
+
+  return(
+      <button className = {`btn ${checkButtonStyle} ${checkButtonState}`} onClick = {onClick} type = {type}>
+        {children}
+      </button>
+  )
+}
+
+
+export default A_Btn
